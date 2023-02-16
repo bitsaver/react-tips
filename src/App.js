018d2d6fe49f1267fb9dead78e8b4a5c87730f19
YangZhenamn <1198790425@qq.com>
2023-02-16 19:54:43 +0800

    Empty project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import Todo from "./componets/Todo"
-
-
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text='first job'/>
-      <Todo text='second job'/>
-      <Todo text='third job'/>
     </div>
   );
 }
